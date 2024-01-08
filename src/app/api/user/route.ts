20bd3b559d4a3d41aaa8b688397a9e7aae92efe3
Valentino Dola <dolavalentino@gmail.com>
2024-01-08 19:25:02 +0000

    update user route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,7 +2,7 @@ import { ConnectToDatabase } from "@/lib/db";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const POST = async (request: { json: () => PromiseLike<{ name: any; email: any; }> | { name: any; email: any; }; }) => {
+export const POST = async (request : Request) => {
     const {email, name} = await request.json();
     await ConnectToDatabase();
     const user = { email, name };
