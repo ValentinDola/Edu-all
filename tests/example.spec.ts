f16468bda877d6072323a36f085ebd4364f54278
Valentino Dola <dolavalentino@gmail.com>
2024-01-22 00:36:33 +0000

    Starting Testing with Playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Career/);
+});
+
+test('should navigate to the ASSESSMENT page', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('http://localhost:3000/');
+  // Find an element with the text 'ASSESSMENT' and click on it
+  await page.getByRole('link', { name: 'ASSESSMENT' }).click();
+  
+})
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
