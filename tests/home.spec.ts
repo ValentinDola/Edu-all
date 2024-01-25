12911182a91d935400ae3642df20336beef314cf
Valentino Dola <dolavalentino@gmail.com>
2024-01-25 09:49:28 +0000

    update some test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -63,4 +63,9 @@ test("system test", async ({ page }) => {
     await page.getByLabel('Profile').click();
     await page.getByLabel('avatar').click();
     await page.getByLabel('Log').click();
+
+    await page.goto('https://edu-all-k8z7.vercel.app/');
+  await page.getByRole('link', { name: 'Universities' }).click();
+  await page.getByPlaceholder('University').click();
+  await page.getByPlaceholder('University').fill('ghana');
 });
